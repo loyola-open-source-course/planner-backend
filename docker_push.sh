7025202fbf130e6f6685926e240636be9987efe0
Alexander Kohonovsky <saniaky@gmail.com>
2019-07-30 18:19:18 -0500

    Update docker_push.sh, build image first.

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t saniaky/planner-api:${TRAVIS_BUILD_NUMBER} .
 docker push saniaky/planner-api:${TRAVIS_BUILD_NUMBER}
