0ff38e5e197420676a9239c4e7658d69907e4310
Alexander Kohonovsky <saniaky@gmail.com>
2019-07-30 18:35:14 -0500

    Update docker_push.sh - add quotes to docker image name

--- docker_push.sh
+++ docker_push.sh
@@ -4,7 +4,7 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Build image
-docker build -t saniaky/planner-api:${TRAVIS_BUILD_NUMBER} .
+docker build -t "saniaky/planner-api:${TRAVIS_BUILD_NUMBER}" .
 
 # Push to repository
-docker push saniaky/planner-api:${TRAVIS_BUILD_NUMBER}
+docker push "saniaky/planner-api:${TRAVIS_BUILD_NUMBER}"
